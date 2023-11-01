da45821c57eecf8f93ca14bcc8e0008f59ddf452
trendhub <andrij19941@ukr.net>
2023-11-01 13:55:22 +0200

    q

--- js/script.js
+++ js/script.js
@@ -41,101 +41,68 @@ function viewProducts() {
 // При завантаженні сторінки виводимо товари
 viewProducts();
 
-// function viewPin() {
-//    fetch(
-//       "https://script.googleusercontent.com/macros/echo?user_content_key=eZnwFs78OzOiBuZDhy2Pxcw2dB55b3FZwzVUEsCwUbpBR8dPcszbCFESfmgFbTc_oM5rBdITQkHvOgSv3ZmvFtb9_7t2Zk9Gm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEBT8ukp-5TzAJaSobJwXPWJruGMg1_BFGBBuy253xJS48LKRdReWuPkPaDqLMI9Rg&lib=M567Z4gvLRO51cnxxvnujP_tDJ3Q6be74https://script.googleusercontent.com/macros/echo?user_content_key=G525CwGX58kAFqZo1JtTt_U-0Pu16uqeVa6OLt5RKB4ZtrsDCxdZxADVQsPIc-hklsJ65VRlMZGZC_32IKCvg4-en8BN8lTKm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEiO1Evecy43yh_P95V_8QbBLRG5d1yHOPW3QZ-3xOzms49p0sZrLPB-0PBAuRdUbt0dAAJNLExRq1w33gQnEbpZfjcqvmxOng&lib=M567Z4gvLRO51cnxxvnujP_tDJ3Q6be74"
-//    )
-//       .then((response) => response.json())
-//       .then((products) => {
 
-//          // Змінюємо струтуру відповіді
-//          products = products.sheetPin;
+let SHEET_ID = '1G5yhh-zlxyy2YCEJGB6YJpsMyIysZMDjQNQofslES6Q'
+let SHEET_TITLE = 'Pinterest';
+let SHEET_RANGE = 'A1:B4'
 
-//          // Перебираємо товари
-//          if (products.length === 0) {
-//             // Виводимо текст, якщо результат пустий
-//             boxPin.innerHTML = `<div class="no-result">Немає товарів</div>`;
-//          } else {
-//             // Наповнюємо змінну масивом в вигляді html
-//             const productsHtml = products.map((product) => {
-//                return `<div class="gallery__item">
-// 					<a href="${PinIMG}" class="gallery__item-hover-box">
-// 						<span class="gallery__item-hash-tag link-hover">#pinterest-link</span>
-// 					</a>
-// 					<img src="${PinIMG}" alt="" class="gallery__item-img">
-// 				</div>`;
-//             });
+let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
 
-//             // Виводимо одним махом
-//             boxPin.innerHTML = productsHtml.join("");
-//          }
-//       });
-// }
-// viewPin();
+fetch(FULL_URL)
+   .then(res => res.text())
+   .then(rep => {
+      let data = JSON.parse(rep.substr(47).slice(0, -2));
+      console.log(data.table.rows[0].c[0].v);
+   })
 
-// function viewPin() {
-//    fetch(
-//       "https://script.google.com/macros/s/AKfycbwAUwdvBYgpRBQ3vczUlgvArjjBIRi0V9HcmZlyd6sSPmyYxMiRlvphTYmayJlUoDut/exec"
-//          .then((response) => response.json())
-//          .then((data) => {
-//             const products = data.sheetPin;
-//             if (products.length === 0) {
-//                boxPin.innerHTML = `<div class="no-result">Немає товарів</div>`;
-//             } else {
-//                const productsHtml = products.map((product) => {
-//                   return `<div class="gallery__item">
-//                      <a href="${product.pinIMG}" class="gallery__item-hover-box">
-//                         <span class="gallery__item-hash-tag link-hover">#pinterest-link</span>
-//                      </a>
-//                      <img src="${product.pinIMG}" alt="" class="gallery__item-img">
-//                   </div>`;
-//                });
-//                boxPin.innerHTML = productsHtml.join("");
-//             }
-//          })
-//          .catch((error) => {
-//             console.error("Error fetching data: ", error);
-//          })
-//    )
-// }
 
-// viewPin();
 
-// $.ajax({
-//    url: 'https://script.google.com/macros/s/AKfycbwAUwdvBYgpRBQ3vczUlgvArjjBIRi0V9HcmZlyd6sSPmyYxMiRlvphTYmayJlUoDut/exec',
-//    method: 'GET',
-//    success: function (data) {
-//       console.log('Success', data);
-//    },
-//    error: function (error) {
-//       console.log('Error', error);
-//    }
-// });
 
-// $.ajax({
-//    url: 'https://script.google.com/macros/s/AKfycbwAUwdvBYgpRBQ3vczUlgvArjjBIRi0V9HcmZlyd6sSPmyYxMiRlvphTYmayJlUoDut/exec',
-//    method: 'GET',
-//    success: function (data) {
-//       console.log('Success', data);
 
-//       const pinArray = data.pin; // Отримання масиву з об'єктами pin
-//       const galleryContainer = document.querySelector('.gallery'); // Отримання контейнера галереї
 
-//       // Перебір об'єктів pin у масиві
-//       pinArray.forEach((pin) => {
-//          const galleryItem = document.createElement('div'); // Створення нового елементу div
-//          galleryItem.classList.add('gallery__item'); // Додавання класу до нового елементу
-//          galleryItem.innerHTML = `
-//             <a href="${pin.pinIMG}" class="gallery__item-hover-box">
-//                <span class="gallery__item-hash-tag link-hover">#pinterest-link</span>
-//             </a>
-//             <img src="${pin.pinIMG}" alt="" class="gallery__item-img">
-//          `; // Додавання HTML-коду до нового елементу
-//          galleryContainer.appendChild(galleryItem); // Додавання нового елементу до контейнера галереї
-//       });
-//    },
-//    error: function (error) {
-//       console.log('Error', error);
-//    }
-// });
 
+
+
+
+// Слідкуємо за категоріями
+if (boxCategory) {
+   boxCategory.onclick = (e) => {
+
+      // Забороняємо стандартний функціонал html
+      e.preventDefault();
+
+      // Витягуємо елемент по якому був клік
+      const el = e.target;
+
+      // Відбираємо дата атрибути цього елементу
+      const elData = el.dataset;
+
+      // Відираємо css клас, щоб по ньому відбирати потрібний нам елемент
+      const elClass = el.classList;
+
+      // Якщо це елемент категорії починаємо робити наш функціонал
+      if (elClass.contains('js-cat')) {
+
+         // Формуємо id категорії
+         const catid = elData.catid;
+
+         // Змінна для товарів
+         let catalog;
+
+         // Дивимося чи потрібно очищувати і просто виводити товари
+         if (catid == 0 || catid == undefined) {
+
+            // Витягуємо дані з api
+            catalog = getProducts();
+
+         } else {
+
+            // Виводимо товари відносно категорії
+            catalog = getProductsByCat(catid);
+         }
+
+         // Виводимо товари
+         viewProducts(catalog);
+      }
+   }
+}
